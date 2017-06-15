4743478ba465be16e16dd96623d853c39a933341
aicjofs <aicjofs@gmail.com>
2017-06-15 13:12:25 -0700

    Added option to see the Memo field in received transactions

--- src/java/com/vaklinov/zcashui/ZCashClientCaller.java
+++ src/java/com/vaklinov/zcashui/ZCashClientCaller.java
@@ -365,6 +365,44 @@ public class ZCashClientCaller
 		return map;
 	}
 	
+    public synchronized String getMemoField(String acc, String txID)
+		throws WalletCallException, IOException, InterruptedException
+	{
+		JsonArray jsonTransactions = this.executeCommandAndGetJsonArray(
+			"z_listreceivedbyaddress", wrapStringParameter(acc));
+			
+        for (int i = 0; i < jsonTransactions.size(); i++)
+        {
+            if (jsonTransactions.get(i).asObject().getString("txid",  "ERROR!").equals(txID))
+            {
+            	if (jsonTransactions.get(i).asObject().get("memo") == null)
+            	{
+            		return null;
+            	}
+            	
+                String MemoHex = jsonTransactions.get(i).asObject().getString("memo", "ERROR!");
+                // Skip empty memos
+                if (MemoHex.startsWith("f60000"))
+                {
+                	return null;
+                }
+                
+                StringBuilder MemoAscii = new StringBuilder("");
+                for (int j = 0; j < MemoHex.length(); j += 2)
+                {
+                    String str = MemoHex.substring(j, j + 2);
+                    if (!str.equals("00")) // Zero bytes are empty
+                    {
+                    	MemoAscii.append((char) Integer.parseInt(str, 16));
+                    }
+                }
+                return MemoAscii.toString();
+            }
+        }
+
+        return null;
+	}
+	
 	
 	public synchronized String getRawTransaction(String txID)
 		throws WalletCallException, IOException, InterruptedException
